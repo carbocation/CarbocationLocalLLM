796f77fc90aa9b2c526a2aac3ca4d645d2b85395
James Pirruccello <james@carbocation.com>
2026-08-17 11:46:29 -0700

    Publish llama binary artifact v0.45.0

--- Package.swift
+++ Package.swift
@@ -9,8 +9,8 @@ let localLlamaBinaryArtifactPath = "Vendor/llama-artifacts/release/llama.xcframe
 let localLlamaBinaryArtifactExists = FileManager.default.fileExists(
     atPath: "\(packageRoot)/\(localLlamaBinaryArtifactPath)"
 )
-let llamaBinaryArtifactURL = ""
-let llamaBinaryArtifactChecksum = ""
+let llamaBinaryArtifactURL = "https://github.com/carbocation/CarbocationLocalLLM/releases/download/v0.45.0/llama.xcframework.zip"
+let llamaBinaryArtifactChecksum = "3e1ad2e9374f7336e2531da109e59f70fee3ad934207d619c5b44be9e849bf7d"
 let llamaBinaryArtifactPath = ProcessInfo.processInfo.environment["CARBOCATION_LOCAL_LLM_BINARY_ARTIFACT_PATH"] ?? ""
 let forceSourceLlama = ProcessInfo.processInfo.environment["CARBOCATION_LOCAL_LLM_FORCE_SOURCE_LLAMA"] == "1"
 
